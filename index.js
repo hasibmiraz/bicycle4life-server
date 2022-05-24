22631cf50ac80e59c5ed8dc085f35b56b196606c
hasibmiraz <hasibmiraz09@gmail.com>
2022-05-24 21:14:57 +0600

    Changed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config';
+require('dotenv').config();
 import { MongoClient, ObjectId, ServerApiVersion } from 'mongodb';
 
 // variable
